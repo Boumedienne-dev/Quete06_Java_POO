cb852ba95cc04333130b8f23139ee7ed2c9848b2
Boumedienne-dev <bmessadia83@gmail.com>
2023-04-14 17:40:10 +0200

    Quete06POO

--- src/main/java/org/example/Classroom.java
+++ src/main/java/org/example/Classroom.java
@@ -1,16 +1,23 @@
 package org.example;
 
-
+/**
+ * Classe représentant une classe de la Wild Code School.
+ */
 public class Classroom {
+
     /**
+     * Méthode principale qui crée plusieurs instances de la classe Wilder et affiche leur description.
      *
-     * @param args
+     * @param args les arguments de la ligne de commande (non utilisés dans cette méthode)
      */
+
     public static void main(String[] args) {
         Wilder wilder1 = new Wilder("Alice", true);
         Wilder wilder2 = new Wilder("Bob", false);
         Wilder wilder3 = new Wilder("Charlie", true);
 
+        // Affiche la description de chaque instance en appelant la méthode whoAmI()
+        System.out.println(wilder1.whoAmI());
         System.out.println(wilder1.whoAmI());
         System.out.println(wilder2.whoAmI());
         System.out.println(wilder3.whoAmI());
